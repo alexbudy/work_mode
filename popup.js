14e44b4389176f5fef233483f715cf00861739e0
Alex Budilovsky <alexbudy@gmail.com>
2016-02-10 13:58:15 -0800

    more feature addons such as better adding of urls

--- popup.js
+++ popup.js
@@ -24,18 +24,75 @@ function setWorkingFlag(flag) {
 }
 
 function saveBlockedSites() {
-	sites = this.value.split("\n")
+	var textArea = document.getElementById("blockedSitesTextArea");
+
+	sites = textArea.value.split("\n")
 	for (var i = 0; i < sites.length; i++) {
+		sites[i] = sites[i].trim()
 		if (!sites[i]) {
 			sites.splice(i, 1)
 			i--
 		}
 	}
 
+	filteredSites = sites.filter(function(item, pos) {
+	    return sites.indexOf(item) == pos;
+	})
+
 	var obj = {}
-	obj['sitesToBlock'] = sites.join(",")
+	obj['sitesToBlock'] = filteredSites.join(",")
 
 	chrome.storage.sync.set(obj)
+}
+
+function addLinkListeners() {
+	var base_url = document.getElementById("base_url_id");
+	var full_url = document.getElementById("full_url_id");
+
+	base_url.addEventListener('click', function() {
+		chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+		    addLineToTextArea(getBaseUrlFromFullUrl(tabs[0].url));
+		    saveBlockedSites()
+		});
+	})
+
+	full_url.addEventListener('click', function() {
+		chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+		    addLineToTextArea(tabs[0].url);
+		    saveBlockedSites()
+		});
+	})
+}
+
+// get base url only
+function getBaseUrlFromFullUrl(fullUrl) {
+	var patt=/^https?:\/\/[^\/]+/i;
+
+	var match = patt.exec(fullUrl)
+
+	if (match != undefined) {
+		console.log(match[0].replace("https://", "").replace("http://", ""))
+		return match[0].replace("https://", "").replace("http://", "")
+	}
+}
+
+// add a line to the text area if line not there 
+function addLineToTextArea(line) {
+	if (line.indexOf("chrome-extension:") == 0) { // special case, dont block chrome extension pages
+		return
+	}
+
+	line  = line.replace("https://", "").replace("http://", "")
+
+	var textArea = document.getElementById("blockedSitesTextArea");
+	sites = textArea.value.split("\n").join(",")
+	if (sites.indexOf(line) == -1) { // not in previous lines
+		if (sites.trim().length == 0) {
+			textArea.value = line
+		} else {
+			textArea.value = textArea.value + "\n" + line
+		}
+	}
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -63,4 +120,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 	textArea.addEventListener("input", saveBlockedSites)
+
+	addLinkListeners()
 }, false);
